b7a2208b4299f0c34190655ecfe3d9ba325083cb
Lucas Zunho <lucaszunho17@gmail.com>
2023-09-07 23:55:35 -0300

    Frequência de palavras & melhora na estrutura do script

--- desafios_alaale/freq_letras.py
+++ desafios_alaale/freq_letras.py
@@ -26,6 +26,10 @@ freq = dict()
 # Conjunto com todas as letras do alfabeto (ASCII) na forma minuscula.
 ascii_chars = set(string.ascii_lowercase)
 
+## @var bool $bl_words
+# Alterna a verificação de frequência das palavras do texto.
+bl_words = True
+
 ##
 # @fn get_text
 # @brief Carrega um arquivo de texto e retorna seu conteúdo.
@@ -40,7 +44,7 @@ def get_text(filepath, verbose=True):
     text = ""
     with open(filepath, 'r') as textfd:
         for line in textfd:
-            line = line.rstrip("\n")
+            line = line.replace("\n", " ")
             if verbose:
                 print("\t\"{}\"".format(line))
             text += line.lower()
@@ -54,17 +58,24 @@ def get_text(filepath, verbose=True):
 # @param[in, out] freq Valores de frequência de cada char presente no texto.
 def char_vals(text):
     global freq
-    
-    # Removendo pontuação
-    for p in string.punctuation:
-        text = text.replace(p, "")
-
-    # Contagem
     for ch in text:
         if ch == " ": continue
         if ch not in freq:
             freq[ch] = 0
         freq[ch] += 1
+
+##
+# @fn word_vals
+# @brief Contagem de palavras e adição no dicionário.
+# 
+# @param text Texto para ser verificado.
+# @param[in,out] freq Valores de frequência das palavras.
+def word_vals(text):
+    global freq
+    for w in text.split(" "):
+        if w not in freq:
+            freq[w] = 0
+        freq[w] += 1
 
 # Entrada de dados
 if len(sys.argv) > 1:
@@ -72,8 +83,13 @@ if len(sys.argv) > 1:
     text = get_text(sys.argv[1])
 else:
     text = str(input("Texto: ")).lower()
+    
+# Removendo pontuação
+for p in string.punctuation:
+    text = text.replace(p, "")
 
 # Contagem de chars
+print("\n=> Contagem de letras...")
 char_vals(text)
 used_chars = set(freq.keys())
 unused_chars = ascii_chars - used_chars
@@ -82,3 +98,12 @@ unused_chars = ascii_chars - used_chars
 for k in sorted(freq.keys()):
     print("{} => {}".format(k, freq[k]))
 print("Letras usadas\t\t=>\t{}\nLetras não usadas\t=>\t{}".format(used_chars, unused_chars))
+
+# Contagem de palavras
+print("\n=> Contagem de palavras...")
+freq = dict() # reset
+word_vals(text)
+
+# stdout
+for k in sorted(freq.keys()):
+    print("{} => {}".format(k, freq[k]))
